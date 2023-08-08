880c9f964cd1155c1f0e174ad1d45fbdd15a8f63
Tom Scott <tubbo@psychedeli.ca>
2023-08-08 12:31:18 -0400

    add basic cli

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use std::path::PathBuf;
+use clap::{Parser,Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Compile a code path from your project and write its binary to a file on disk.
+    Build {
+        /// A code path to compile, like 'foo::Bar.baz()'
+        path: String,
+        /// A file path on disk to write the binary to
+        file: PathBuf,
+    },
+
+    /// Compile a code path from your project and run it immediately.
+    Run {
+        /// A code path to compile, like 'foo::Bar.baz()'
+        path: String,
+    },
+
+    /// Start a new REPL session.
+    Console {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::Build { path, file } => {
+            // let program = compiler::compile(path);
+            // let mut output = std::fs::File::create(file);
+
+            // output.write_all(program);
+            println!("built {} at {}", path, file.display());
+        }
+        Commands::Run { path } => {
+            println!("running {}...", path)
+
+            // let program = compiler::compile(path);
+
+            // vm::run(program);
+        }
+        Commands::Console => {
+            // TODO: repl
+        }
+    }
 }
