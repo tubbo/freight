9cd8921e90594a7f2f5bd0d042094583145c0ebc
Tom Scott <tubbo@psychedeli.ca>
2023-08-10 02:34:18 -0400

    move functionality to library

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use std::path::PathBuf;
+
+/// Build the given code path to a binary file on disk.
+pub fn build(path: &String, file: &PathBuf) {
+    println!("built '{}' to {}", path, file.display());
+}
+
+/// Run a code path for the current project.
+pub fn run(path: &String) {
+    println!("running '{}'", path);
+}
+
+/// Publish a package to the Freight registry.
+pub fn publish(name: &String, version: &String) {
+    println!("published package '{}' at {}", name, version);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,57 @@
-use std::path::PathBuf;
 use clap::{Parser,Subcommand};
+use std::path::PathBuf;
 
+/// Command-line interface to the Freight toolchain.
 #[derive(Debug, Parser)]
 #[command(author, version, about, long_about = None)]
 struct Cli {
     #[command(subcommand)]
     command: Commands,
+
+    #[arg(short, long)]
+    /// Show all messages from compiler output.
+    verbose: bool,
+
+    #[arg(short, long)]
+    /// Only show error messages from compiler output.
+    quiet: bool,
 }
 
 #[derive(Debug, Subcommand)]
+/// Commands for the Freight toolchain CLI.
 enum Commands {
     /// Compile a code path from your project and write its binary to a file on disk.
     Build {
-        /// A code path to compile, like 'foo::Bar.baz()'
+        /// A code path to compile, like 'foo::Bar.baz()'.
         path: String,
-        /// A file path on disk to write the binary to
+        /// A file path on disk to write the binary to.
         file: PathBuf,
     },
 
     /// Compile a code path from your project and run it immediately.
     Run {
-        /// A code path to compile, like 'foo::Bar.baz()'
+        /// A code path to compile, like 'foo::Bar.baz()'.
         path: String,
     },
 
-    /// Start a new REPL session.
-    Console {},
+    /// Publish a package to the Freight registry.
+    Publish {
+        /// Name of the package to publish.
+        package: String,
+
+        /// Version of the package to publish. This must be unique, or you will
+        /// receive an error from the registry.
+        version: String,
+    },
 }
+
 
 fn main() {
     let cli = Cli::parse();
 
     match &cli.command {
-        Commands::Build { path, file } => {
-            // let program = compiler::compile(path);
-            // let mut output = std::fs::File::create(file);
-
-            // output.write_all(program);
-            println!("built {} at {}", path, file.display());
-        }
-        Commands::Run { path } => {
-            println!("running {}...", path)
-
-            // let program = compiler::compile(path);
-
-            // vm::run(program);
-        }
-        Commands::Console => {
-            // TODO: repl
-        }
+        Commands::Build { path, file } => freight::build(path, file),
+        Commands::Run { path } => freight::run(path),
+        Commands::Publish { package, version } => freight::publish(package, version),
     }
 }
